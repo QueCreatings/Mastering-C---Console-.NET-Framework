203b67887b3cc8c6c957d4c561a2c1ae913ade46
QueCreatings <qhamahlana@gmail.com>
2025-12-05 23:12:34 +0200

    Integer Declarations

--- Mastering C# Console .NET Framework/Program.cs
+++ Mastering C# Console .NET Framework/Program.cs
@@ -10,6 +10,17 @@ namespace Mastering_C__Console.NET_Framework
     {
         static void Main(string[] args)
         {
+            // Fundamental Numeric Data Types 
+
+            // Integer Types
+            sbyte num = 8;                             // has 8 bits
+            byte nums = 8;
+            short number = 16;                         // has 16 bits
+            ushort numbers = 16;                        
+            int integer = 32;                          // has 32 bits
+            uint integers = 32;
+            long digit = 64;                           // has 64 bits
+            ulong digits = 64;    
         }
     }
 }
